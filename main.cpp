cd2a45f3d75273cf5195054ffaee0e6e3d2c731d
Jeff Chien <jeffchienmail@gmail.comn>
2011-11-02 23:36:41 -0700

    Got it to load with a jpeg image.  Fixed my name

--- main.cpp
+++ main.cpp
@@ -1,30 +1,39 @@
 #include <stdlib.h>
 #include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
 
-void DrawPixel(SDL_Surface *screen, Uint8 R, Uint8 G, Uint8 B, int x, int y);
+#define WINDOW_TITLE    "Out of Sight 0.0.0.0.0.0.0.0.0.0.1"
+#define DIE(...)        { fprintf(stderr, __VA_ARGS__); exit(1); }
 
 int main(int argc, char *argv[])
 {
     if(SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0)
-    {
-        fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
-        exit(1);
-    }
+        DIE("Unable to init SDL: %s\n", SDL_GetError());
     atexit(SDL_Quit);
 
-    SDL_Surface *screen = SDL_SetVideoMode(640, 480, 16, SDL_SWSURFACE);
+    SDL_Surface *screen = SDL_SetVideoMode(640, 480, 16, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    SDL_WM_SetCaption(WINDOW_TITLE, 0);
     if(screen == NULL)
-    {
-        fprintf(stderr, "Unable to set 640x480 video: %s\n", SDL_GetError());
-        exit(1);
-    }
+        DIE("Unable to set 640x480 video: %s\n", SDL_GetError());
 
-    int x, y;
-    for(x = 10; x < 20; x++)
-    {
-        for(y = 10; y < 20; y++)
-            DrawPixel(screen, 255, 255, 255, x, y);
-    }
+    SDL_Surface *temp, *sprite;
+    temp = IMG_Load("assets/girl-stick-figure.jpg");
+    if(temp == NULL)
+        DIE("Unable to load sprite\n");
+    sprite = SDL_DisplayFormatAlpha(temp);
+    SDL_FreeSurface(temp);
+
+    SDL_Rect src;
+    src.x = 0;
+    src.y = 0;
+    src.w = 300;
+    src.h = 400;
+
+    SDL_Rect dest;
+    dest.x = 20;
+    dest.y = 20;
+    dest.w = 300;
+    dest.h = 400;
 
     int done = 0;
     SDL_Event event;
@@ -49,59 +58,13 @@ int main(int argc, char *argv[])
 
             default:
                 break;
-        }   // End switch        
+        }   // End switch
+
+        SDL_BlitSurface(sprite, &src, screen, &dest);
+        SDL_Flip(screen);
     }   // End while
 
+    SDL_FreeSurface(sprite);
     return(0);
-}
-
-void DrawPixel(SDL_Surface *screen, Uint8 R, Uint8 G, Uint8 B, int x, int y)
-{
-    Uint32 color = SDL_MapRGB(screen->format, R, G, B);
-
-    if ( SDL_MUSTLOCK(screen) ) {
-        if ( SDL_LockSurface(screen) < 0 ) {
-            return;
-        }
-    }
-    switch (screen->format->BytesPerPixel) {
-        case 1: { /* Assuming 8-bpp */
-            Uint8 *bufp;
-
-            bufp = (Uint8 *)screen->pixels + y*screen->pitch + x;
-            *bufp = color;
-        }
-        break;
-
-        case 2: { /* Probably 15-bpp or 16-bpp */
-            Uint16 *bufp;
-
-            bufp = (Uint16 *)screen->pixels + y*screen->pitch/2 + x;
-            *bufp = color;
-        }
-        break;
-
-        case 3: { /* Slow 24-bpp mode, usually not used */
-            Uint8 *bufp;
-
-            bufp = (Uint8 *)screen->pixels + y*screen->pitch + x;
-            *(bufp+screen->format->Rshift/8) = R;
-            *(bufp+screen->format->Gshift/8) = G;
-            *(bufp+screen->format->Bshift/8) = B;
-        }
-        break;
-
-        case 4: { /* Probably 32-bpp */
-            Uint32 *bufp;
-
-            bufp = (Uint32 *)screen->pixels + y*screen->pitch/4 + x;
-            *bufp = color;
-        }
-        break;
-    }
-    if ( SDL_MUSTLOCK(screen) ) {
-        SDL_UnlockSurface(screen);
-    }
-    SDL_UpdateRect(screen, x, y, 1, 1);
 }
 
